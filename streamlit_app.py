f2778e853845de7ff8fdb048257c306939d1bd40
snowperi <106560518+snowperi@users.noreply.github.com>
2022-05-31 11:19:30 -0500

    Update streamlit_app.py
    Added the pandas , imported a file and created a data frame.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,13 @@
 import streamlit
+import pandas
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorrites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale Spinach & Rocket Smoothy')
 streamlit.text('Hard Boiled Free Range Egg')
 streamlit.text('Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
