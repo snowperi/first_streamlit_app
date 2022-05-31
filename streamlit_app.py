5040dafef1090365c4d015ef63b977ca6aa0d82b
snowperi <106560518+snowperi@users.noreply.github.com>
2022-05-31 10:58:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale Spinach & Rocket Smoothy')
 streamlit.text('Hard Boiled Free Range Egg')
-streamlit.text('U+1F347 Avacado Toast')
+streamlit.text(U+1F347 'Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
