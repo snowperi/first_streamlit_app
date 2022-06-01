22837c51b25b3ae0587514f048ffce81e705f0b4
snowperi <106560518+snowperi@users.noreply.github.com>
2022-06-01 17:11:32 -0500

    Update streamlit_app.py
    Added the text box for user input

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,9 @@ streamlit.dataframe(fruits_to_show)
 #New Section to display fruitvice api response
 import requests
 streamlit.header('Fruityvise Fruit Advice!')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'kiwi')
+streamlit.write('The user entered', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
